a0e818715f0ca43a19e89e66e19c647f665563ce
David Lehnartz <malermeister-lehnartz@gmx.de>
2024-09-26 18:11:50 +0200

    added functions

--- script.js
+++ script.js
@@ -4,21 +4,21 @@
 
 
 // Array for all notes
-/* let allNotes = {
+let allNotes = {
     'notesTitle': [],
     'notes': [],
     'archiveNotesTitle': [],
     'archiveNotes': [],
     'trashNotesTitle': [],
     'trashNotes': [],
-} */
+}
 
-let notes = [];
+/* let notes = []; */
 
 
 // Initialize
 function init() {
-
+    isInputEmpty();
 }
 
 
@@ -26,7 +26,7 @@ function init() {
 function renderNotes() {
     let noteContentRef = document.getElementById('notes');
     noteContentRef.innerHTML = '';
-    for (let indexNotes = 0; indexNotes < notes.length; indexNotes++) {
+    for (let indexNotes = 0; indexNotes < allNotes.notes.length; indexNotes++) {
         noteContentRef.innerHTML += getNotesTemplate(indexNotes);
     }
 }
@@ -34,10 +34,37 @@ function renderNotes() {
 
 // Add Title and Notes
 function addTitleAndNote() {
+    let inputNoteTitleRef = document.getElementById('input_title');
+    let inputNoteTitle = inputNoteTitleRef.value;
     let inputNoteRef = document.getElementById('input_note');
     let inputNote = inputNoteRef.value;
 
-    notes.push(inputNote);
+    allNotes.notes.push(inputNote);
+    allNotes.notesTitle.push(inputNoteTitle);
 
     renderNotes();
+    clearInput();
+    isInputEmpty();
+    console.table(allNotes.notes);
+    console.table(allNotes.notesTitle);
+}
+
+
+// If Input Is Empty Don`t Submit
+function isInputEmpty() {
+    let inputNoteTitle = document.getElementById('input_title').value;
+    let inputNote = document.getElementById('input_note').value;
+
+    if (inputNoteTitle === "" || inputNote === "") {
+        document.getElementById('button').disabled = true;
+    } else {
+        document.getElementById('button').disabled = false;
+    }
+}
+
+
+// Clear Input
+function clearInput() {
+    document.getElementById('input_title').value = "";
+    document.getElementById('input_note').value = "";
 }
